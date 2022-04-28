b72e69e64b4e9d985aa15c2b5d852d677df72d58
nevermin9 <chizzza@gmail.com>
2022-04-28 11:44:43 +0800

    just add grid box to the gallery for future improvement

--- src/pages/Gallery/index.vue
+++ src/pages/Gallery/index.vue
@@ -1,9 +1,34 @@
 <template>
-    <h1>
-        i am gallery
-    </h1>
+    <section class="gallery" >
+        <h1>
+            i am gallery
+        </h1>
+        <router-link :to="{ name: 'Home' }">
+            home
+        </router-link>
 
-    <router-link :to="{ name: 'Home' }">
-        home
-    </router-link>
+        <article class="gallery__grid-box">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </article>
+    </section>
 </template>
+
+<style lang="scss">
+.gallery {
+    &__grid-box {
+        display: grid;
+        grid-template-rows: repeat(3, 1fr);
+        grid-template-columns: repeat(3, 1fr);
+        gap: 15px;
+        max-width: 700px;
+    }
+}
+</style>
